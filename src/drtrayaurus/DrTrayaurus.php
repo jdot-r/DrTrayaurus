fb6e658f3188c6c632b26dd9ee5121436066c50c
TheDeibo <deiboibbs@gmail.com>
2016-02-03 13:52:07 +0000

    Create DrTrayaurus.php

---
+++ src/drtrayaurus/DrTrayaurus.php
@@ -0,0 +1,165 @@
+<?php
+namespace drtrayaurus;
+use pocketminecommandCommand;
+use pocketminecommandCommandExecutor;
+use pocketminecommandCommandSender;
+use pocketmineeventListener;
+use pocketmineeventplayerPlayerQuitEvent;
+use pocketmineeventserverDataPacketSendEvent;
+use pocketminenetworkprotocolAddMobPacket;
+use pocketminenetworkprotocolAddPlayerPacket;
+use pocketminenetworkprotocolMoveEntityPacket;
+use pocketminenetworkprotocolMovePlayerPacket;
+use pocketminenetworkprotocolRemoveEntityPacket;
+use pocketminenetworkprotocolRemovePlayerPacket;
+use pocketminePlayer;
+use pocketminepluginPluginBase;
+class DrTrayaurus extends PluginBase implements Listener, CommandExecutor{
+     @var  DisguiseSession[] 
+    public $e;
+     @var  MobStore 
+    private $mobStore;
+    public function onEnable(){
+        $this-e = [];
+        $this-getServer()-getPluginManager()-registerEvents($this, $this);
+        $this-mobStore = new MobStore($this);
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if(isset($args[1])){
+            if($sender-hasPermission(drtrayaurus.other)){
+                if(($p = $this-getServer()-getPlayer($args[1])) instanceof Player){
+                    if($this-isDisguised($p-getID())){
+                        $this-destroyDisguise($p-getID());
+                        $sender-sendMessage(DrTrayaurus Disguise closed for  . $p-getName());
+                        $p-sendMessage(Your disguise has been closed.);
+                        return true;
+                    }
+                    else{
+                        if(is_numeric($args[0])) {
+                            $s = new DisguiseSession($p, $args[0]);
+                            $this-e[$p-getID()] = $s;
+                            $sender-sendMessage(DrTrayaurus Disguise activated for  . $p-getName());
+                            $p-sendMessage(You now have a disguise.);
+                        }
+                        elseif(($mob = $this-getMobStore()-getMobId($args[0])) !== false){
+                            $s = new DisguiseSession($p, $mob);
+                            $this-e[$p-getID()] = $s;
+                            $sender-sendMessage(DrTrayaurus Disguise activated for  . $p-getName());
+                            $p-sendMessage(You now have a disguise.);
+                        }
+                        else{
+                            $sender-sendMessage(No mob found with that name.);
+                        }
+                        return true;
+                    }
+                }
+                else{
+                    $sender-sendMessage(Player not found.);
+                    return true;
+                }
+            }
+            else{
+                $sender-sendMessage(You do not have permission to disguise others.);
+                return true;
+            }
+        }
+        else{
+            if($sender instanceof Player){
+                if($this-isDisguised($sender-getID())){
+                    $this-destroyDisguise($sender-getID());
+                    $sender-sendMessage(DrTrayaurus Disguise closed.);
+                    return true;
+                }
+                else{
+                    if(isset($args[0])){
+                        if(is_numeric($args[0])) {
+                            $s = new DisguiseSession($sender, $args[0]);
+                            $this-e[$sender-getID()] = $s;
+                            $sender-sendMessage(DrTrayaurus Disguise activated.);
+                        }
+                        elseif(($mob = $this-getMobStore()-getMobId($args[0])) !== false){
+                            $s = new DisguiseSession($sender, $mob);
+                            $this-e[$sender-getID()] = $s;
+                            $sender-sendMessage(Disguise activated.);
+                        }
+                        else{
+                            $sender-sendMessage(Thats not DrTrayaurus Name!);
+                        }
+                        return true;
+                    }
+                    else{
+                        $sender-sendMessage(You need to specify the right DrTrayaurus);
+                        return true;
+                    }
+                }
+            }
+            else{
+                $sender-sendMessage(You need to specify a player.);
+                return true;
+            }
+        }
+    }
+    public function onPacketSend(DataPacketSendEvent $event){
+        if(isset($event-getPacket()-eid)){
+            if($this-isDisguised($event-getPacket()-eid) && !$event-getPlayer()-hasPermission(drtrayaurus.exempt)){
+              if($event-getPacket() instanceof MovePlayerPacket){
+                      $pk = new MoveEntityPacket;
+                      $pk-entities = [[$event-getPacket()-eid, $event-getPacket()-x, $event-getPacket()-y, $event-getPacket()-z, $event-getPacket()-yaw, $event-getPacket()-pitch]];
+                      $event-getPlayer()-dataPacket($pk);
+                      $event-setCancelled();
+              }
+              elseif($event-getPacket() instanceof AddPlayerPacket){
+                      $pk = new AddMobPacket;
+                      $pk-eid = $event-getPacket()-eid;
+                      $pk-type = $this-e[$event-getPacket()-eid]-getType();
+                      $pk-x = $event-getPacket()-x;
+                      $pk-y = $event-getPacket()-y;
+                      $pk-z = $event-getPacket()-z;
+                      $pk-pitch = $event-getPacket()-pitch;
+                      $pk-yaw = $event-getPacket()-yaw;
+                      $pk-metadata = [];
+                      $event-getPlayer()-dataPacket($pk);
+                      $event-setCancelled();
+              }
+              elseif($event-getPacket() instanceof RemovePlayerPacket){
+                      $pk = new RemoveEntityPacket;
+                      $pk-eid = $event-getPacket()-eid;
+                      $event-getPlayer()-dataPacket($pk);
+                      $event-setCancelled();
+              }
+           }
+        }
+    }
+    public function isDisguised($eid){
+        return (isset($this-e[$eid]));
+    }
+    public function onQuit(PlayerQuitEvent $event){
+        if($this-isDisguised($event-getPlayer()-getID())){
+            $this-destroyDisguise($event-getPlayer()-getID());
+        }
+    }
+    public function onDisable(){
+        $this-getLogger()-info(Closing disguise sessions.);
+        foreach($this-e as $eid = $s){
+            $this-destroyDisguise($eid);
+        }
+    }
+    public function destroyDisguise($i){
+        if(isset($this-e[$i])){
+            $this-e[$i]-despawnDisguise();
+            $this-e[$i]-revertNameTag();
+            $p = $this-e[$i]-getPlayer();
+            unset($this-e[$i]);
+            $p-spawnToAll();
+        }
+    }
+    
+      @return MobStore
+     
+    public function getMobStore(){
+        return $this-mobStore;
+    }
+    public function getResourcePath(){
+        return $this-getFile() . resources;
+    }
+}
